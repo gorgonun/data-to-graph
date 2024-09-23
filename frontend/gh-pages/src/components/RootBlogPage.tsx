57c47db72a4e3434d4935cfa50f5abc2406e04c5
Patrick <polariusvulcano@gmail.com>
2024-09-23 19:34:17 -0300

    Blog list page

--- frontend/gh-pages/src/components/RootBlogPage.tsx
+++ frontend/gh-pages/src/components/RootBlogPage.tsx
@@ -1,5 +1,12 @@
 import { BlogFontmatter } from "@/libs/content";
-import { Card, CardActionArea, CardMedia, Stack } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  Stack,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Props {
   sortedPostData: ({
@@ -10,29 +17,37 @@ interface Props {
 
 export default function RootBlogPage({ sortedPostData }: Props) {
   return (
-    <Stack>
-      {sortedPostData.map((post) => {
-        return (
-          <Card key={post.id}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image={`blog/${post.contextPath}/${post.coverImage}`}
-                alt={post.coverImageAlt}
-              />
-            </CardActionArea>
-          </Card>
-        );
-      })}
-      <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            image={sortedPostData[0].coverImage}
-            alt={sortedPostData[0].coverImageAlt}
-          />
-        </CardActionArea>
-      </Card>
+    <Stack sx={{ width: "100%", maxWidth: "lg", mx: "auto" }}>
+      <Stack mt={2}>
+        {sortedPostData.map((post) => {
+          const T = require(`@/content/blog/${post.contextPath}/${post.coverImage}`);
+
+          return (
+            <Stack key={post.id} p={2} width="30vw" height='50vh'>
+              <Card>
+                <CardActionArea component={Link} href={`/blog/${post.id}`}>
+                  <Stack alignItems='center' p={2}>
+                    <Stack>
+                      <Image
+                        src={T}
+                        alt={post.coverImageAlt}
+                        style={{
+                          width: 'auto',
+                          maxHeight: '30vh'
+                        }}
+                      />
+                    </Stack>
+                    <Stack mt={1}>
+                      <Typography fontWeight={700} fontSize='1em'>{post.title}</Typography>
+                      <Typography fontSize='0.8em' color='gray'>{post.subtitle}</Typography>
+                    </Stack>
+                  </Stack>
+                </CardActionArea>
+              </Card>
+            </Stack>
+          );
+        })}
+      </Stack>
     </Stack>
   );
 }
